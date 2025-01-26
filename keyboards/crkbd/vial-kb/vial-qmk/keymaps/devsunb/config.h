c2d06843d519f8ba286e34f4579ebabefcb5028b
Jaeseok Lee <devsunb@gmail.com>
2025-01-26 13:08:51 +0900

    update keymap

--- keyboards/crkbd/vial-kb/vial-qmk/keymaps/devsunb/config.h
+++ keyboards/crkbd/vial-kb/vial-qmk/keymaps/devsunb/config.h
@@ -9,8 +9,8 @@
 #define MOUSEKEY_DELAY             0
 #define MOUSEKEY_INTERVAL          16
 #define MOUSEKEY_MOVE_DELTA        1
-#define MOUSEKEY_MAX_SPEED         32
-#define MOUSEKEY_TIME_TO_MAX       48
+#define MOUSEKEY_MAX_SPEED         16
+#define MOUSEKEY_TIME_TO_MAX       32
 #define MOUSEKEY_WHEEL_DELAY       0
 #define MOUSEKEY_WHEEL_INTERVAL    64
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 64
--- keyboards/crkbd/vial-kb/vial-qmk/keymaps/devsunb/keymap.c
+++ keyboards/crkbd/vial-kb/vial-qmk/keymaps/devsunb/keymap.c
@@ -1,50 +1,61 @@
 #include QMK_KEYBOARD_H
 
-#define NUM_RGU LT(1, KC_RGUI)
-#define TG_MOUS TG(2)
-#define MO_RGB  MO(3)
+enum layers {
+    L_BASE,
+    L_NAV,
+    L_MOUSE,
+    L_RGB,
+};
+
+enum keycodes {
+    CMD_SPC = SAFE_RANGE,
+    OS_SFT,
+    OS_CMD,
+    OS_ALT,
+};
+
+#define LT_RCMD LT(L_NAV, KC_RCMD)
+#define TG_MOU  TG(L_MOUSE)
+#define MO_RGB  MO(L_RGB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_split_3x6_3_ex2(
+  [L_BASE] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  MO_RGB,     MO_RGB,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_ESC,       KC_B,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_ESC,    XXXXXXX,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RBRC,
+       OS_SFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RBRC,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, KC_LGUI,  KC_SPC,     KC_ENT, NUM_RGU, KC_BSPC
+                                           OS_CMD,  KC_SPC,  OS_ALT,     KC_ENT, KC_BSPC, LT_RCMD
                                       //`--------------------------'  `--------------------------'
   ),
 
-  // NUM
-  [1] = LAYOUT_split_3x6_3_ex2(
+  [L_NAV] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6, _______,    _______,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, KC_MPRV, KC_MNXT, KC_MPLY, XXXXXXX, _______,    XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, TG_MOUS,  KC_GRV,
+      _______, KC_VOLD, KC_VOLU, KC_MUTE, CW_TOGG, XXXXXXX, _______,    _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  TG_MOU,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE, XXXXXXX,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_BSLS, CW_TOGG,
+      _______, KC_MPRV, KC_MNXT, KC_MPLY, XXXXXXX, XXXXXXX,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_BSLS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
-  // MOUSE
-  [2] = LAYOUT_split_3x6_3_ex2(
+  [L_MOUSE] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, _______,    _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______, _______,    XXXXXXX, MS_LEFT, MS_DOWN,   MS_UP, MS_RGHT, TG_MOUS, XXXXXXX,
+      _______, _______, _______, _______, _______, _______, _______,    _______, MS_LEFT, MS_DOWN,   MS_UP, MS_RGHT,  TG_MOU, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,                      MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, XXXXXXX, XXXXXXX,
+      _______, _______, _______, _______, _______, _______,                      MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, CMD_SPC, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    MS_BTN1, MS_BTN2, MS_BTN3
                                       //`--------------------------'  `--------------------------'
   ),
 
-  // RGB
-  [3] = LAYOUT_split_3x6_3_ex2(
+  [L_RGB] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
       XXXXXXX, UG_HUED, UG_HUEU, UG_PREV, UG_NEXT, XXXXXXX, _______,    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -56,3 +67,99 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   )
 };
+
+typedef enum {
+    os_up_unqueued,
+    os_up_queued,
+    os_down_unused,
+    os_down_used,
+} oneshot_state;
+
+oneshot_state os_alt_state = os_up_unqueued;
+oneshot_state os_cmd_state = os_up_unqueued;
+oneshot_state os_sft_state = os_up_unqueued;
+
+bool is_oneshot_ignored_key(uint16_t keycode) {
+    switch (keycode) {
+    case OS_ALT:
+    case OS_CMD:
+    case OS_SFT:
+    case LT_RCMD:
+        return true;
+    default:
+        return false;
+    }
+}
+
+void update_oneshot(
+    oneshot_state *state,
+    uint16_t mod,
+    uint16_t trigger,
+    uint16_t keycode,
+    keyrecord_t *record
+) {
+    if (keycode == trigger) {
+        if (record->event.pressed) {
+            if (*state == os_up_unqueued) {
+                register_code(mod);
+            }
+            *state = os_down_unused;
+        } else {
+            switch (*state) {
+            case os_down_unused:
+                *state = os_up_queued;
+                break;
+            case os_down_used:
+                *state = os_up_unqueued;
+                unregister_code(mod);
+                break;
+            default:
+                break;
+            }
+        }
+    } else {
+        if (!record->event.pressed) {
+            if (!is_oneshot_ignored_key(keycode)) {
+                switch (*state) {
+                case os_down_unused:
+                    *state = os_down_used;
+                    break;
+                case os_up_queued:
+                    *state = os_up_unqueued;
+                    unregister_code(mod);
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    update_oneshot(
+        &os_alt_state, KC_LALT, OS_ALT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_cmd_state, KC_LCMD, OS_CMD,
+        keycode, record
+    );
+    update_oneshot(
+        &os_sft_state, KC_LSFT, OS_SFT,
+        keycode, record
+    );
+
+    if (keycode == CMD_SPC) {
+        if (record->event.pressed) {
+            register_code(KC_LCMD);
+            register_code(KC_SPC);
+        } else {
+            unregister_code(KC_SPC);
+            unregister_code(KC_LCMD);
+            layer_off(L_MOUSE);
+        }
+    }
+
+    return true;
+}
